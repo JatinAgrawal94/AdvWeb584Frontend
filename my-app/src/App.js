42a56af81259eae26a1a4cef352984fca8cfcd98
Jatin Agrawal <jatinagrawal0801@gmail.com>
2022-12-18 18:18:38 -0800

    some changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -20,7 +20,7 @@ function App() {
   dotenv.config();
   return (
     <Router>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"/>
 
     <div className="width-full">
         <Nav/>
--- my-app/src/components/authorizationComponent.jsx
+++ my-app/src/components/authorizationComponent.jsx
@@ -5,8 +5,11 @@ export default function AuthorizationComponent({children}){
     try {
         const {userInfo}=useSelector(state=>state.userSignin);
         verify(userInfo.token,"this is my custom Secret key for authentication");
+        console.log("Its going good here");
         return children;
       } catch (error) {
           window.location.href="/login"
+          console.log("Authorizaton Error");
+          console.log(error);
     }
 }
