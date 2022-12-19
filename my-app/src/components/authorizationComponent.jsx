ed3aa3a4aecd2d5dc9b18f4994ce360b43a95f6c
Jatin Agrawal <jatinagrawal0801@gmail.com>
2022-12-18 19:03:59 -0800

    some changes

--- my-app/src/components/authorizationComponent.jsx
+++ my-app/src/components/authorizationComponent.jsx
@@ -3,7 +3,9 @@ import { useSelector } from 'react-redux';
 
 export default function AuthorizationComponent({children}){
     try {
+        console.log("1");
         const {userInfo}=useSelector(state=>state.userSignin);
+        console.log("2");
         verify(userInfo.token,"this is my custom Secret key for authentication");
         console.log("Its going good here");
         return children;
