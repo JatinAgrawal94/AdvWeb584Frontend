e7cb0cbcaf7a1a0af5950911ae749a0ed20f453f
Jatin Agrawal <jatinagrawal0801@gmail.com>
2022-12-20 17:59:58 -0800

    some changes

--- my-app/src/components/home.jsx
+++ my-app/src/components/home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 export function Home(){
     return (
         <div>
-            This application would help us to book appointements for patients. There would be two user roles. 1) Patient 2) Person who books an Appointment.
+            This application would help us to book appointements for patients.
         </div>
     )
 }
